c81ecbf5b752fbb939e9b450905e3798687c18ac
Ryan Jonathan S <94353686+mike42s@users.noreply.github.com>
2022-03-16 13:00:50 +0700

    Versi Final No 1

--- c14190008_1/bin/c14190008_1.dart
+++ c14190008_1/bin/c14190008_1.dart
@@ -1,3 +1,18 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  int? a;
+  String? N;
+  stdout.write("Masukkan Angka : ");
+  N = stdin.readLineSync();
+  int? angka = int.parse(N!);
+  stdout.write("Hasil Faktor : ");
+  for (int i = angka; i > 0; i--) {
+    if (angka % i == 0) {
+      stdout.write("$i");
+      if (i > 1) {
+        stdout.write(", ");
+      }
+    }
+  }
 }

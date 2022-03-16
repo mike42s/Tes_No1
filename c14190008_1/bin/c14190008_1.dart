c9973134a4294e7df37b0f894ea467c082e9ccb8
Ryan Jonathan S <94353686+mike42s@users.noreply.github.com>
2022-03-16 13:01:37 +0700

    Fix No 1

--- c14190008_1/bin/c14190008_1.dart
+++ c14190008_1/bin/c14190008_1.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  int? a;
   String? N;
   stdout.write("Masukkan Angka : ");
-  N = stdin.readLineSync();
-  int? angka = int.parse(N!);
+  N = stdin.readLineSync(); //input
+  int? angka = int.parse(N!); //String to angka
   stdout.write("Hasil Faktor : ");
   for (int i = angka; i > 0; i--) {
     if (angka % i == 0) {
